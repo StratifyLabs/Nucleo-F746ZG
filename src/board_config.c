31ea8eecd4c26f4ce368ed494a591d9f45a7d8f6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-08-26 20:17:12 -0600

    Added LWIP and ethernet

--- src/board_config.c
+++ src/board_config.c
@@ -22,7 +22,13 @@ limitations under the License.
 #include <mcu/debug.h>
 #include <cortexm/task.h>
 #include <sos/link/types.h>
+
+#include "lwip_config.h"
 #include "board_config.h"
+
+#define MANAGE_APPLICATION 1
+
+extern void SystemClock_Config();
 
 #define TRACE_COUNT 8
 #define TRACE_FRAME_SIZE sizeof(link_trace_event_t)
@@ -35,39 +41,66 @@ const ffifo_config_t board_trace_config = {
 };
 ffifo_state_t board_trace_state;
 
-extern void SystemClock_Config();
-
 
 void board_trace_event(void * event){
-	link_trace_event_header_t * header = event;
+	link_trace_event_t * trace_event = event;
 	devfs_async_t async;
 	const devfs_device_t * trace_dev = &(devfs_list[0]);
+
 
 	//write the event to the fifo
 	memset(&async, 0, sizeof(devfs_async_t));
 	async.tid = task_get_current();
 	async.buf = event;
-	async.nbyte = header->size;
+	async.nbyte = trace_event->header.size;
 	async.flags = O_RDWR;
 	trace_dev->driver.write(&(trace_dev->handle), &async);
+
+	if( trace_event->header.id == LINK_NOTIFY_ID_POSIX_TRACE_EVENT ){
+
+		//this will reset the device if the application crashes
+		if( trace_event->posix_trace_event.posix_event_id == LINK_POSIX_TRACE_FATAL ){
+			//mcu_core_reset(0,0);
+		}
+	}
+
+}
+
+void start_rtc(){
+	int fd = open("/dev/rtc", O_RDWR);
+	int result;
+	if( fd < 0 ){
+		mcu_debug_log_warning(MCU_DEBUG_USER0, "failed to open rtc");
+		return;
+	}
+
+	if( (result = ioctl(fd, I_RTC_SETATTR)) < 0 ){
+		mcu_debug_log_warning(MCU_DEBUG_USER0, "failed to set rtc attributes (%d, %d)", result, errno);
+		return;
+	}
+
+	close(fd);
 }
 
 void board_event_handler(int event, void * args){
 	switch(event){
 		case MCU_BOARD_CONFIG_EVENT_ROOT_TASK_INIT:
+			//no debugging allowed here
+			break;
+
+		case MCU_BOARD_CONFIG_EVENT_FATAL:
+			mcu_debug_log_error(MCU_DEBUG_SYS, "Fatal Error %s\n", (const char*)args);
 			break;
 
 		case MCU_BOARD_CONFIG_EVENT_ROOT_FATAL:
 			//start the bootloader on a fatal event
-			//mcu_core_invokebootloader(0, 0);
 			if( args != 0 ){
-				mcu_debug_log_error(MCU_DEBUG_SYS, "Fatal Error %s", (const char*)args);
+				mcu_debug_log_error(MCU_DEBUG_SYS, "Fatal Error %s\n", (const char*)args);
 			} else {
-				mcu_debug_log_error(MCU_DEBUG_SYS, "Fatal Error unknown");
+				mcu_debug_log_error(MCU_DEBUG_SYS, "Fatal Error unknown\n");
 			}
-			while(1){
-				;
-			}
+
+			mcu_core_invokebootloader(0, 0);
 			break;
 
 		case MCU_BOARD_CONFIG_EVENT_ROOT_INITIALIZE_CLOCK:
@@ -78,8 +111,18 @@ void board_event_handler(int event, void * args){
 			break;
 
 		case MCU_BOARD_CONFIG_EVENT_START_LINK:
-			mcu_debug_log_info(MCU_DEBUG_USER1, "Start LED %d");
+			mcu_debug_log_info(MCU_DEBUG_USER0, "Start Link");
 			sos_led_startup();
+
+			start_rtc();
+
+			//start LWIP
+			mcu_debug_log_info(MCU_DEBUG_USER0, "Start LWIP");
+
+			usleep(500*1000);
+			lwip_api.startup(&lwip_api);
+
+
 			break;
 
 		case MCU_BOARD_CONFIG_EVENT_START_FILESYSTEM:
--- src/link_config.c
+++ src/link_config.c
@@ -22,7 +22,6 @@ limitations under the License.
 #include <mcu/debug.h>
 #include <sos/dev/pio.h>
 #include "link_config.h"
-#include "config.h"
 
 #if !defined SOS_BOARD_RX_FIFO_WORDS
 #define SOS_BOARD_RX_FIFO_WORDS 128
@@ -71,37 +70,21 @@ link_transport_driver_t link_transport = {
 	.close = sos_link_transport_usb_close,
 	.wait = sos_link_transport_usb_wait,
 	.flush = sos_link_transport_usb_flush,
-	.timeout = 500
+	.timeout = 500,
+	.o_flags = 0,
+	.transport_read = link1_transport_slaveread,
+	.transport_write = link1_transport_slavewrite
 };
 
 static usbd_control_t m_usb_control;
 
-#if defined SOS_BOARD_USB_PORT
-const usbd_control_constants_t link_transport_usb_constants = {
-	.handle.port = SOS_BOARD_USB_PORT,
-	.handle.config = 0,
-	.handle.state = 0,
-	.device =  &sos_link_transport_usb_dev_desc,
-	.config = &sos_link_transport_usb_cfg_desc,
-	.string = &sos_link_transport_usb_string_desc,
-	.class_event_handler = sos_link_usbd_cdc_event_handler
-};
-#else
-#define link_transport_usb_constants sos_link_transport_usb_constants
-#endif
-
 link_transport_phy_t link_transport_open(const char * name, const void * options){
 	usb_attr_t usb_attr;
 	link_transport_phy_t fd;
-	MCU_UNUSED_ARGUMENT(options);
 
 	//set up the USB attributes
 	memset(&(usb_attr.pin_assignment), 0xff, sizeof(usb_pin_assignment_t));
-#if defined SOS_BOARD_USB_ATTR_FLAGS
-	usb_attr.o_flags = SOS_BOARD_USB_ATTR_FLAGS;
-#else
 	usb_attr.o_flags = USB_FLAG_SET_DEVICE;
-#endif
 	usb_attr.pin_assignment.dp = SOS_BOARD_USB_DP_PIN;
 	usb_attr.pin_assignment.dm = SOS_BOARD_USB_DM_PIN;
 	usb_attr.freq = mcu_board_config.core_osc_freq;
@@ -114,16 +97,16 @@ link_transport_phy_t link_transport_open(const char * name, const void * options
 	usb_attr.tx_fifo_word_size[4] = SOS_BOARD_TX4_FIFO_WORDS; //TX endpoint 4
 	usb_attr.tx_fifo_word_size[5] = SOS_BOARD_TX5_FIFO_WORDS; //TX endpoint 5
 
-	mcu_debug_log_info(MCU_DEBUG_USER1, "Open USB");
+	mcu_debug_log_info(MCU_DEBUG_LINK, "Open USB");
 
 	fd = sos_link_transport_usb_open(name,
 												&m_usb_control,
-												&link_transport_usb_constants,
+												&sos_link_transport_usb_constants,
 												&usb_attr,
 												mcu_pin(0xff,0xff),
 												1); //USB pin is active high
 
-	mcu_debug_log_info(MCU_DEBUG_USER1, "Returned %d", fd);
+	mcu_debug_log_info(MCU_DEBUG_LINK, "Open USB");
 
 	return fd;
 }
